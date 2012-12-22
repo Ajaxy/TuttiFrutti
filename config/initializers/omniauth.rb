26ca8b3a9fe8da234acf44bb34eff5f45db43a07
alder <alderkireyev@gmail.com>
2012-12-23 03:06:05 +0400

    refractor omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 # encoding: utf-8
+config = YAML.load_file(File.join(Rails.root, 'config', 'omniauth.yml'))[Rails.env]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :vkontakte, "3311082", "QaNTqvRLNaycYCw0NMJG", display: "popup"
-  provider :facebook, "322346714548331", "2f4e79066b88dc772e46baa74273fa15", display: "popup"
-  # provider :vkontakte, "3304189", "EaR3uNBqo1f7QXpdps7X", display: "popup"
-  # provider :facebook, "317434528372827", "4c13d36e4f7fe07ca373c40e64953ba8", display: "popup"
+  provider :vkontakte, config['vkontakte']['app_id'], config['vkontakte']['secret']
+  provider :facebook , config['facebook' ]['app_id'], config['facebook' ]['secret']
 end
