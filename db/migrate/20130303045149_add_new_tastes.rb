28097bb98c536cc2a585a96ff991f8f8ed9a5542
Andrey Mereskin <mereskin@gmail.com>
2013-03-05 08:13:58 -0800

    removed migration

--- db/migrate/20130303045149_add_new_tastes.rb
+++ db/migrate/20130303045149_add_new_tastes.rb
@@ -6,6 +6,9 @@ class AddNewTastes < ActiveRecord::Migration
 
     Taste.find(5).update_attributes(:soy => true)
     Taste.find(38).update_attributes(:soy => true)
+
+    Taste.create(:code => "Oreo", :sort_order => 52, :name => "Печенье с кремом", :color => "935e26", :yogurt => "зеленое яблоко, ваниль, банан", :fruits => "банан, клубника, голубика", :liquid => "карамель, шоколад", :topings => "ореховая смесь, дробленый арахис, йогуртовые чипсы")
+    Taste.create(:code => "Blueberry_Cheese_Cake", :sort_order => 11, :name => "Черничный чизкейк", :color => "4a5d91", :yogurt => "классический, клубника, малина", :fruits => "голубика, клубника, киви", :liquid => "клубника, шоколад", :topings => "мотчи, ореховая смесь, кокос")
   end
 
   def down
