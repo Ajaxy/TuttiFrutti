73455c9b52164731cef1ab37798908766fb7becb
Ajaxy <alx.zinchuk@gmail.com>
2014-02-12 14:52:34 +0400

    Новые селекты в адресах

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -10,25 +10,36 @@ function showMap () {
         items = [],
         geoObjects = window.geoObjects = {};
 
-    $('#tools #cities').on('change', function () {
-        var id = $(this).find(':selected').val();
+    $('#tools #spots')
+        .on('change', function () {
+            var id = $(this).find(':selected').val().match(/\d+/),
+                spot = CONTACTS[id];
 
-        map.setBounds(CITIES[id].bounds, CITIES[id].zoom, { duration: 300 });
-    });
-
-    $('#tools #spots').on('change', function () {
-        var id = $(this).find(':selected').val();
-
-        map.setCenter(CONTACTS[id].coords, 14);
-        setTimeout(function () {
-            geoObjects[id].balloon.open();
-        }, 0);
-    });
+            if (spot.type == 'city') {
+                map.setBounds(spot.bounds, spot.zoom, { duration: 300 });
+            } else {
+                map.setCenter(spot.coords, 14);
+                setTimeout(function () {
+                    geoObjects[id].balloon.open();
+                }, 0);
+            }
+        })
+        .find('option').each(function (i, o) {
+            if ($(o).val().match('city')) {
+                $(o).css('fontWeight', 'bold');
+            } else {
+                $(o).css('paddingLeft', '20px').end();
+            }
+        });
 
     $.each(CONTACTS, function (i, contact) {
+        if (contact.type == 'city') {
+            return;
+        }
+
         var geoObject = new ymaps.Placemark(contact.coords, {
-                iconContent: contact.title,
-                balloonContentHead: contact.title,
+                iconContent: contact.name,
+                balloonContentHead: contact.name,
                 balloonContentBody: contact.desc
             }, {
                 preset: 'islands#greenStretchyIcon'
