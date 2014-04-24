8812846500249d93c7a367394a1699ea9b29506f
Ajaxy <alx.zinchuk@gmail.com>
2014-04-24 12:45:09 +0400

    reorder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140422131405) do
+ActiveRecord::Schema.define(:version => 20140424083756) do
 
   create_table "tastes", :force => true do |t|
     t.string   "code"
